77e7c6031af61787406280eed75db74c639e948b
rgorman <rgorman1@unl.edu>
2015-12-30 00:28:59 -0600

    Addiitons and corrections to .R script for sWords with levels.

--- R_files/code/sWord_levels/extracting_sWord_levels.R
+++ R_files/code/sWord_levels/extracting_sWord_levels.R
@@ -63,11 +63,47 @@ for (i in 1:length(files.v))  {
 
 
 
+# Convert sWord.freq.table.list into a data matrix. This allows for further analysis and manipulation.
 
-View(sWord.table)
-View(sWord.freq.table)
 
-length(sWord.freq.table.list[[2]])
+# First, create list object (freqs.l) containing data.frame object with columns containing ID number for source file
+# variable name (i.e., the sWord), and relative frequency of sWord
 
+freqs.l <- mapply(data.frame, ID=seq_along(sWord.freq.table.list),
+                  sWord.freq.table.list, SIMPLIFY=FALSE, MoreArgs=list(stringsAsFactors=FALSE))
+
+# Next, combine this list of data.frame objects into a single large data.frame object.
+
+freqs.df <- do.call(rbind, freqs.l)
+
+# check names of data frame to provide parameters for conversion to wide format table
+names(freqs.df)
+
+#convert from long form table to wide format (i.e., the files will be represented by rows, the sWords by columns)
+result <- xtabs(Freq ~ ID+sWord.contents, data=freqs.df)
+
+#convert wide format table to matrix object
+final.m <- apply(result, 2, as.numeric)
+
+# make names for rows
+names_for_files.v <- gsub (".xml", "", files.v)
+
+
+# substitute files names for row numbers
+rownames(final.m) <-names_for_files.v
+
+rownames(final.m)
+
+
+
+
+# reduce data matrix to features with largest means (most common features)
+smaller.m <- final.m[, apply(final.m,2,mean)>=.025]
+
+# check size of reduced feature set
+dim(smaller.m)
+
+
+# From here smaller.m can be used as basis for clustering and classification algorithms.
 
 

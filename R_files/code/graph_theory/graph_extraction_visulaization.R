946e64c97205ae4afc7161ee34739f414514fcfc
Robert Gorman <rgorman1@unl.edu>
2016-04-01 17:46:14 -0500

    Work on script to extract graphs from tree files.

--- R_files/code/graph_theory/graph_extraction_visulaization.R
+++ R_files/code/graph_theory/graph_extraction_visulaization.R
@@ -1,5 +1,72 @@
+# A script to extract data from treebanks and input it to igraph and associated routines.
 
 require(igraph)
+require(XML)
+
+# this line reads an XML file into an R object
+doc.object <- xmlTreeParse(file = "../graph_theory/source.xml")
+
+
+# this line reads doc.object into an XML Node object which can be accessed through subsetting
+top <- xmlRoot(doc.object)
+
+length(top)
+top[[1]]
+top[[1]][1]
+a <- xmlSApply(top[[1]], function(x) xmlGetAttr(x, "id"))
+class(a)
+a <- as.numeric(a)
+b <- as.numeric(b)
+b <- xmlSApply(top[[1]], function(x) xmlGetAttr(x, "head"))
+c <- matrix(a, ncol = 1)
+d <- cbind(c, b)
+
+length(which(d[,2] > 0))
+e <- matrix(nrow = length(which(d[, 2] >0)), ncol = 2)
+
+e <- d[which(d[, 2] > 0), ]
+
+class(e)
+for (i in 1: nrow(d)) {
+  if (d[i,2] > 0) {
+    
+    
+  }
+
+}
+
+
+length(xmlSApply(top[[1]], function(x) xmlGetAttr(x, "id")))
+
+i <- 7
+
+for (i in i:length(top)) {
+  a <- xmlSApply(top[[i]], function(x) xmlGetAttr(x, "postag"))
+  a <- as.vector(a)
+  # record number of verbs in vector "a"
+  b <- length(grep("v", a))
+  # record number of tokens in sentence less punctuation
+  c <- length(a) - length(grep("u---", a))
+  # ratio of number of verbs to total tokens (less punctuation)
+  d <-append(d, b/c)
+  # number of verbs in sentence
+  e <- append (e, b)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################
 
 # read edge matrix in from csv file
 a <- read.csv(file = "../graph_theory/testGraph_1.csv", header = FALSE)

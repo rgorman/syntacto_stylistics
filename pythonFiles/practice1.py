3119f02f56a11045fc86ffc8711a5ed3ad9655b8
rgorman <rgorman1@unl.edu>
2016-05-22 00:40:53 -0500

    Python practice.

---
+++ pythonFiles/practice1.py
@@ -0,0 +1,22 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+def main():
+ # today = date.today()
+ # print "Today's date is ", today
+  
+ # print "Date Components", today.day, today.month, today.year
+  
+# print "Today's weekday number", today.weekday()
+  
+  today = datetime.now()
+  print today
+
+if __name__ == "__main__":  
+  main()  
+
+  
+  
+
+

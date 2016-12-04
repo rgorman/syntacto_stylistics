6604061433c9ff4ad21e977e9a178efce8e2d41f
rgorman <rgorman1@unl.edu>
2016-12-04 15:08:17 -0600

    Classification results with 35-token chunks.

--- R_files/code/sWord_levels/Shortened_sWordLevels_chunks_Dec-2-2016.R
+++ R_files/code/sWord_levels/Shortened_sWordLevels_chunks_Dec-2-2016.R
@@ -30,7 +30,7 @@ for (i in 1:length(files.v)) {
   
   # here we must split files into chunks
   
-  divisor <- length(word.nodes)/40
+  divisor <- length(word.nodes)/35
   max.length <- length(word.nodes)/divisor
   x <- seq_along(word.nodes)
   chunks.l <- split(word.nodes, ceiling(x/max.length))
@@ -155,11 +155,177 @@ object.size(freqs.df)
 
    
 
-
+sum(lengths(sWord.freq.table.list)) == nrow(freqs.df)
 
 
 # if sums are correct, clean memory
 rm(chunks.l, doc.object, freqs.l, sWord.freq.table.list, sWord.nodes.l)
+
+
+########################################
+
+
+
+
+# set variables for loop
+
+i <- 1 # index variable for loop
+rows.v <- 50000 # number of rows to be used in xtab
+total.rows.v <- nrow(freqs.df)
+y <-     floor(nrow((freqs.df))/rows.v)
+seg.v <- seq_along(1:y)
+row.counter.v <- NULL
+border.row.v <- NULL
+
+
+timestamp()
+for (i in 1:y) {
+  
+  if (i == 1) {
+    
+    indexA <- which(freqs.df[,3] == freqs.df[(seg.v[i] * rows.v),3])
+    input.df <- freqs.df[1:indexA[length(indexA)],]
+    border.row.v <- append(border.row.v, indexA[length(indexA)])
+    row.counter.v <- append(row.counter.v, nrow(input.df))
+    
+    nam <- paste("result", i, ".R", sep = "")
+    nam2 <- paste("final.df", i, ".R", sep = "")
+    
+    
+    output.df <-  xtabs(Freq ~ ID+combined.content, data=input.df)
+    
+    final.df <- as.data.frame.matrix(output.df)
+    
+    meany <- matrix(colMeans(final.df), nrow = 1)
+    
+    rm(input.df)
+    
+    
+    # save(output.df, file = nam) # apparently unneccessary
+    save(final.df, file = nam2)
+    print("fuck")
+    
+    
+    rm(nam, nam2, output.df, final.df)
+    
+    
+  }
+  
+  else {
+    
+    if (i != length(seg.v)) {
+      
+      indexA <- which(freqs.df[,3] == freqs.df[border.row.v[length(border.row.v)] + 1,3])
+      indexB <- which(freqs.df[,3] == freqs.df[((seg.v[i]) * rows.v) ,3])
+      input.df <- freqs.df[indexA[1]:indexB[length(indexB)],]
+      border.row.v <- append(border.row.v, indexA[1])
+      border.row.v <- append(border.row.v, indexB[length(indexB)])
+      row.counter.v <- append(row.counter.v, nrow(input.df))
+      
+      
+      
+      nam <- paste("result", i, ".R", sep = "")
+      nam2 <- paste("final.df", i, ".R", sep = "")
+      
+      
+      output.df <-  xtabs(Freq ~ ID+combined.content, data=input.df)
+      rm(input.df)
+      
+      final.df <- as.data.frame.matrix(output.df)
+      
+      beany <- matrix(colMeans(final.df), nrow = 1)
+      meany <- rbind(meany, beany)
+      
+      # save(output.df, file = nam)
+      save(final.df, file = nam2)
+      print("piss")
+      
+      
+      rm(nam, nam2, output.df, final.df)
+      
+      
+    }
+    
+    else {
+      
+      indexA <- which(freqs.df[,3] == freqs.df[border.row.v[length(border.row.v)] + 1,3])
+      input.df <- freqs.df[indexA[1]: total.rows.v, ]
+      border.row.v <- append(border.row.v, indexA[1])
+      
+      
+      row.counter.v <- append(row.counter.v, nrow(input.df))
+      
+      
+      
+      nam <- paste("result", i, ".R", sep = "")
+      nam2 <- paste("final.df", i, ".R", sep = "")
+      
+      
+      output.df <-  xtabs(Freq ~ ID+combined.content, data=input.df)
+      rm(input.df)
+      
+      final.df <- as.data.frame.matrix(output.df)
+      
+      beany <- matrix(colMeans(final.df), nrow = 1)
+      meany <- rbind(meany, beany)
+      
+      # save(output.df, file = nam)
+      save(final.df, file = nam2)
+      
+      print("poop")
+      
+      
+      rm(nam, nam2, output.df, final.df)
+      
+      
+    }
+    
+  }
+  
+  
+}  
+
+timestamp()
+
+# check to see if we have correct number of rows
+sum(row.counter.v) == nrow(freqs.df)
+
+
+# extract vector of column means from our matrix of column means
+freq.mean.v <- colMeans(meany)
+
+#collect column means of a given magnitude
+keepers.v <- which(freq.mean.v >=.0025)
+
+
+i <- 1
+timestamp()
+for (i in 1:y) {
+  if (i == 1) {
+    
+    nam2 <- paste("final.df", i, ".R", sep = "")
+    load(nam2)
+    selected.df <- final.df[, keepers.v]
+    
+    rm(final.df)
+    
+  }
+  else {
+    
+    nam2 <- paste("final.df", i, ".R", sep = "")
+    load(nam2)
+    selected.df2 <- final.df[, keepers.v]
+    selected.df <- rbind(selected.df, selected.df2)
+    
+    rm(final.df, selected.df2)
+    
+  }
+  
+  
+}
+timestamp()
+
+
 
 
 
@@ -192,7 +358,7 @@ for (i in 1:length(files.v))  {
   
   # here we must split files into chunks
   
-  divisor <- length(word.nodes)/40
+  divisor <- length(word.nodes)/35
   max.length <- length(word.nodes)/divisor
   x <- seq_along(word.nodes)
   chunks.l <- split(word.nodes, ceiling(x/max.length))
@@ -208,7 +374,8 @@ sum(chunk.total) == nrow(selected.df)
 
 sum(chunk.total)
 
-
+sum(row.counter.v)
+border.row.v
 
 
 # this series of nested loops uses chunk.total to produce a vector (ID.holder) containing the correct set of chunk
@@ -245,15 +412,17 @@ for (i in 1:length(chunk.total)) {
 
 # replace row names in final.df with names of chunks from ID.holder
 ID.matrix <- matrix(ID.holder, ncol = 1)
-write.csv(ID.matrix, file = "Results_Nov-2016/Authors_AllGreekFiles_40tokens_Nov-28.csv")
+write.csv(ID.matrix, file = "Results_Nov-2016/Authors_AllGreekFiles_25tokens_Dec-3.csv")
 
 
 row.names(selected.df) <- ID.holder
 
 # take a look
-View(final.df[, 1:5])
+View(selected.df[, 1:5])
 
-object.size(final.df)
+object.size(selected.df)
+
+
 
 
 
@@ -267,10 +436,12 @@ object.size(final.df)
 ordered.df <- selected.df[, order(colMeans(selected.df), decreasing=TRUE)]
 
 # create smaller.df to correspond to variable names in classificaiton script
-smaller.df <- ordered.df
+smaller.df1 <- ordered.df
+smaller.df2 <- ordered.df
+
 
 #save data frame object to .csv file
-write.csv(ordered.df, file = "Results_Nov-2016/AllGreekFiles_40tokens_Dec-2.csv")
+write.csv(ordered.df, file = "Results_Nov-2016/AllGreekFiles_35tokens_Dec-2.csv")
 save(final.df, file = "Results_Nov-2016/AllGreekFiles_100tokens_Nov-24.R")
 
 
---
+++ R_files/code/sWord_levels/two_classifications.R
@@ -0,0 +1,152 @@
+
+require(e1071)
+require(gmodels)
+require(klaR)
+
+
+# read meta data from disk; 
+chunk.ratios.m <- read.csv(file="Results_Nov-2016/MetaData_AllGreek_35Tokens.csv", stringsAsFactors = FALSE, header = TRUE)
+dim(chunk.ratios.m)
+chunk.ratios.m[,2]
+View(chunk.ratios.m)
+
+
+
+# read in author.v for list of levels
+author.factor <- chunk.ratios.m[, 1]
+unique(author.factor)
+
+
+# make empty objects for results of classification tests
+svm.results.l <- list()
+svm.error.matrix.l <- list()
+testing.classes.l <- list()
+
+i <- 1
+
+timestamp()
+for (i in 1:100) {
+  
+  
+  
+  #create vector of random integers = 10% of obs in smaller.df
+  testing.index.v <- sample (seq (1, nrow(smaller.df)), 1641, prob = chunk.ratios.m[, 2])
+  
+  
+  #create training and testing data matrices using testing.index.v and its inverse
+  testing.data <- smaller.df[testing.index.v, ]
+  training.data <- smaller.df[-testing.index.v, ]
+  
+  #create vectors of factors giving classes (here = authors) of each row in testing.data and training.data
+  training.classes <- as.factor(author.factor[-testing.index.v])
+  testing.classes <- as.factor(author.factor[testing.index.v])
+  
+  
+  model.svm <- svm(training.data, training.classes, kernel = "linear", scale = FALSE)
+  
+  svm.results.l[[i]] <- predict(model.svm, testing.data)
+  svm.error.matrix.l[[i]] <- errormatrix(testing.classes, svm.results.l[[i]])
+  
+  
+  
+}
+
+timestamp()
+
+#combine all matrices contained in err.matr.l into one matrix for export to .csv file
+a <- do.call(rbind, svm.error.matrix.l)
+
+
+save(svm.error.matrix.l, file="Results_NOv-2016/svmErrorMatrix_35tokens_Dec-4-2016.R")
+save(svm.results.l, file="Results_Nov-2016/svmResults_35tokens_Dec-4-2016.R")
+write.csv(a, file = "Results_Nov-2016/svmError_Spreadsheet_35tokens_Dec-4-2016.csv")
+
+
+
+
+
+# make empty objects for results of classification tests
+svm.results.l <- list()
+svm.error.matrix.l <- list()
+testing.classes.l <- list()
+
+i <- 1
+
+timestamp()
+for (i in 1:100) {
+  
+  
+  
+  #create vector of random integers = 10% of obs in smaller.df
+  testing.index.v <- sample (seq (1, nrow(smaller.df)), 1641, prob = chunk.ratios.m[, 2])
+  
+  
+  #create training and testing data matrices using testing.index.v and its inverse
+  testing.data <- smaller.df[testing.index.v, ]
+  training.data <- smaller.df[-testing.index.v, ]
+  
+  #create vectors of factors giving classes (here = authors) of each row in testing.data and training.data
+  training.classes <- as.factor(author.factor[-testing.index.v])
+  testing.classes <- as.factor(author.factor[testing.index.v])
+  
+  
+  model.svm <- svm(training.data, training.classes, kernel = "linear", scale = FALSE)
+  
+  svm.results.l[[i]] <- predict(model.svm, testing.data)
+  svm.error.matrix.l[[i]] <- errormatrix(testing.classes, svm.results.l[[i]])
+  
+  
+  
+}
+
+timestamp()
+
+#combine all matrices contained in err.matr.l into one matrix for export to .csv file
+b <- do.call(rbind, svm.error.matrix.l)
+
+
+save(svm.error.matrix.l, file="Results_NOv-2016/svmErrorMatrix_35tokens_Dec-4-2016.R")
+save(svm.results.l, file="Results_Nov-2016/svmResults_35tokens_Dec-4-2016.R")
+write.csv(b, file = "Results_Nov-2016/svmError_Spreadsheet_35tokens_Dec-4-2016.csv")
+
+
+
+
+View(a)
+
+sum(a[,13])
+
+
+sum(a[,13])/2
+
+250*100
+
+
+(143500-(sum(a[,13])/2))/143500
+
+summary(model.svm)
+model.svm$
+dim(model.svm$decision.values)
+
+dim(model.svm$SV)
+View(model.svm$SV)
+
+length(svm.error.matrix.l)
+svm.error.matrix.l[[1]]
+svm.results.l[[1]]
+recheck <- svm(training.data, training.classes, kernel = "linear", scale = FALSE)
+
+
+
+
+save(svm.error.matrix.l, file="Results_NOv-2016/svmErrorMatrix_40tokens_Dec-3-2016.R")
+save(svm.results.l, file="Results_Nov-2016/svmResults_40tokens_Dec-3-2016.R")
+write.csv(a, file = "Results_Nov-2016/svmError_Spreadsheet_40tokens_Dec-3-2016.csv")
+
+
+save(svm.error.matrix.l, file="Results_Sept-2016/scaled-svmErrorMatrix_500tokens_Oct-1-2016.R")
+save(svm.results.l, file="Results_Sept-2016/scaled-svmResults_500tokens_Oct-1-2016.R")
+write.csv(a, file = "Results_Sept-2016/scaled-svmError_Spreadsheet_500tokens_Oct-1-2016.csv")
+
+
+
